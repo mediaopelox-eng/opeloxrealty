05ec42bebb2051fe8a70c5636d6677ef52858e1b
mediaopelox-eng <media.opelox@gmail.com>
2026-05-30 12:55:12 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
-fetch("layout/head.html")
-.then(res => res.text())
-.then(data => {
-  document.getElementById("head").insertAdjacentHTML("beforeend", data);
-});
+// fetch("layout/head.html")
+// .then(res => res.text())
+// .then(data => {
+//   document.getElementById("head").insertAdjacentHTML("beforeend", data);
+// });
 
 fetch("layout/header.html")
 .then(res => res.text())
@@ -37,3 +37,58 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+
+const counters = document.querySelectorAll('.counter');
+
+const startCounter = () => {
+
+  counters.forEach(counter => {
+
+    const target = +counter.getAttribute('data-target');
+    const speed = 200;
+    const increment = target / speed;
+
+    const updateCounter = () => {
+
+      const current = +counter.innerText;
+
+      if(current < target){
+
+        counter.innerText = Math.ceil(current + increment);
+
+        setTimeout(updateCounter, 10);
+
+      } else {
+
+        if(target === 76){
+          counter.innerText = target + '%';
+        } else {
+          counter.innerText = target + '+';
+        }
+
+      }
+
+    };
+
+    updateCounter();
+
+  });
+
+};
+
+const statsSection = document.querySelector('.stats');
+
+let started = false;
+
+window.addEventListener('scroll', () => {
+
+  const sectionTop = statsSection.offsetTop - 500;
+
+  if(window.scrollY > sectionTop && !started){
+
+    startCounter();
+    started = true;
+
+  }
+
+});
